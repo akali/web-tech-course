7ad9f8a8ebd9335af2775db28bf62dfe958fba9c
Kali Aisultan <aisultan.kali@gmail.com>
2019-04-16 17:34:53 +0600

    implement views

--- labs/todo/api/views.py
+++ labs/todo/api/views.py
@@ -1,19 +1,24 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 
 # Create your views here.
+from api.models import TaskList, Task
+
 
 def get_task_lists(request):
-    pass
+    result = [TaskList.objects.all()]
+    result = [x.to_json() for x in result]
+    return JsonResponse(result, safe=False)
 
 
 def get_task_list(request, id):
-    pass
+    return JsonResponse(TaskList.objects.get(id=id).to_json())
 
 
 def get_tasks_of_task_list(request, id):
-    pass
+    return JsonResponse([x.to_json for x in TaskList.objects.get(id=id).task_set], safe=False)
 
 
 def get_task(request, id):
-    pass
+    return JsonResponse(Task.objects.get(id=id).full(), safe=False)
