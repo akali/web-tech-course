7986068def5f0a547289727a450610b039178ff2
Kali Aisultan <aisultan.kali@gmail.com>
2019-04-16 17:41:28 +0600

    fix Task model

--- labs/todo/api/models.py
+++ labs/todo/api/models.py
@@ -21,7 +21,7 @@ class Task(models.Model):
     name = models.CharField('name', max_length=200)
     created_at = models.DateTimeField('created_at', auto_now=True)
     due_on = models.DateTimeField('due_on', auto_now=True)
-    status = models.CharField('status', default='TODO')
+    status = models.CharField('status', default='TODO', max_length=200)
     task_list = models.ForeignKey(TaskList, on_delete=models.CASCADE)
 
     def full(self):
