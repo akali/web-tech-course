349399e7e8d37bcf3e51542595181738335e8173
Kali Aisultan <aisultan.kali@gmail.com>
2019-04-16 17:19:36 +0600

    add models

--- labs/todo/api/models.py
+++ labs/todo/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class TaskList(models.Model):
+    name = models.CharField('name', max_length=200)
+
+
+class Task(models.Model):
+    name = models.CharField('name', max_length=200)
+    created_at = models.DateTimeField('created_at', auto_now=True)
+    due_on = models.DateTimeField('due_on', auto_now=True)
+    status = models.CharField('status', default='TODO')
+    task_list = models.ForeignKey(TaskList, on_delete=models.CASCADE)
