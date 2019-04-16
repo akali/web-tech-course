2a5d620bbb79dfa88e45db6eaf4cc48a31ee8f70
Kali Aisultan <aisultan.kali@gmail.com>
2019-04-16 17:22:27 +0600

    add default methods

--- labs/todo/api/models.py
+++ labs/todo/api/models.py
@@ -6,6 +6,16 @@ from django.db import models
 class TaskList(models.Model):
     name = models.CharField('name', max_length=200)
 
+    def full(self):
+        obj = {
+            'id': self.id,
+            'name': self.name,
+        }
+        return obj
+
+    def to_json(self):
+        return self.full()
+
 
 class Task(models.Model):
     name = models.CharField('name', max_length=200)
@@ -13,3 +23,21 @@ class Task(models.Model):
     due_on = models.DateTimeField('due_on', auto_now=True)
     status = models.CharField('status', default='TODO')
     task_list = models.ForeignKey(TaskList, on_delete=models.CASCADE)
+
+    def full(self):
+        obj = {
+            'id': self.id,
+            'name': self.name,
+            'created_at': self.created_at,
+            'due_on': self.due_on,
+            'status': self.status,
+            'task_list': self.task_list,
+        }
+        return obj
+
+    def to_json(self):
+        obj = {
+            'id': self.id,
+            'name': self.name,
+        }
+        return obj
