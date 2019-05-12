62ea16f079dcaeef4568d67f02e4acd6694e386f
Aisultan Kali <aisultan.kali@gmail.com>
2019-05-12 22:00:11 +0600

    add routes

--- project/django/store/market/views.py
+++ project/django/store/market/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def welcome_page(request):
+    return None
+
+
+def category(request):
+    return None
+
+
+def category_items(request):
+    return None
+
+
+def item(request):
+    return None
+
+
+def item_detail(request):
+    return None
+
+
+def like(request):
+    return None
+
+
+def comment(request):
+    return None
+
