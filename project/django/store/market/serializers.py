3518558a62dd22377ac7175223fd7527a42283cf
elina <elinar.98@gmail.com>
2019-05-12 22:52:37 +0600

    add serializer

--- project/django/store/market/serializers.py
+++ project/django/store/market/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from rest_framework import serializers
-from .models import Category
+from .models import Category, Like
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -9,34 +9,11 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'email',)
 
-# class PostSerializer(serializers.Serializer):
-#     title = serializers.CharField(required=True)
-#     body = serializers.CharField(required=True)
-#     like_count = serializers.IntegerField()
-#     created_at = serializers.DateTimeField()
-#     created_by = UserSerializer(read_only=True)
-#
-#     def create(self, validated_data):
-#         post = Post(**validated_data)
-#         post.save()
-#         return post
-#
-#     def update(self, instance, validated_data):
-#         instance.body = validated_data.get('body', instance.body)
-#         instance.save()
-#         return instance
-#
-#
-# class PostSerializer2(serializers.ModelSerializer):
-#     title = serializers.CharField(required=True)
-#     body = serializers.CharField(required=True)
-#     like_count = serializers.IntegerField()
-#     created_at = serializers.DateTimeField()
-#     created_by = UserSerializer(read_only=True)
-#
-#     class Meta:
-#         model = Post
-#         fields = ('title', 'body', 'like_count', 'created_at', 'created_by')
-#         # fields = '__all__'
-#
-#
+
+class LikeSerializer(serializers.ModelSerializer):
+    item = serializers.ItemSerializer(read_only=True)
+    author = serializers.UserSerializer(read_only=True)
+
+    class Meta:
+        model = Like
+        fields = ('author', 'item', )
--- project/django/store/market/views/auth.py
+++ project/django/store/market/views/auth.py
@@ -7,8 +7,7 @@ from rest_framework.decorators import api_view
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated, AllowAny
-
-from market.serializers import UserSerializer
+from store.market.serializers import UserSerializer
 
 
 class UserList(generics.ListAPIView):
