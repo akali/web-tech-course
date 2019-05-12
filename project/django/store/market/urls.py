d44f18d8efe3e35f0312e8f91a94b53a1bb2d8cf
aida <america125>
2019-05-12 23:50:01 +0600

    rename

--- project/django/store/market/urls.py
+++ project/django/store/market/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import views
+from .views import views, cbv_category
 
 urlpatterns = [
     path('', views.welcome_page),
-    path('category', views.category),
+    # path('category', views.category),
+    path('category', cbv_category.CategoryApi.as_view(), name='task_lists'),
     path('category/<int:pk>', views.category_items),
     path('item', views.item),
     path('item/<int:pk>', views.item_detail),
